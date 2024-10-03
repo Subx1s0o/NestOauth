6b3a2bb844899d936c4734dd84640dcc85e49505
Subx1s0o <nazarsabardin@gmail.com>
2024-10-03 15:52:25 +0300

    feat: add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from 'libs/src/config/config.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule, AuthModule],
+  imports: [ConfigModule, AuthModule, UserModule],
 })
 export class AppModule {}
