9f9f2319895a2a0c2131a143ed7311ea1c5865f3
Subx1s0o <nazarsabardin@gmail.com>
2024-10-05 05:43:00 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const logger: Logger = new Logger('NestApplication');
   const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000'], credentials: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://front-nest-passport-js.vercel.app',
+    ],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
   logger.log(`The Server started at PORT ${PORT}`);
